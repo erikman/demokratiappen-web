93e9d9e1df1f8c562c013c5fefdc1f11aa38247a
Erik Man <goodbyte@gmail.com>
2014-05-05 22:46:25 +0200

    MOre WIP

--- app/js/data.js
+++ app/js/data.js
@@ -47,17 +47,45 @@ Event.valueByTag = function(type) {
 };
 
 
-var Projection = function(targetType) {
-  this.targetType = targetType;
+/**
+ * @brief Class for working with graph data
+ *
+ */
+var Graph = function() {
+  this.nodes = [];
+  this.edges = [];
 }
+
+Graph.prototype.addEdge(from, to, distance) {  
+}
+
+Graph.prototype.calculateDistances() {
+}
+
+Graph.prototype.distanceFrom(a) {
+  return this.nodes[a].distances;
+}
+
+TypeProjection.totalGraph = new Graph();
+TypeProjection.totalGraph.addEdge(
+  {name: 'Fredrik Reinfeldt', type:'Person'},
+  {name: 'Moderaterna', type:'Political Party'}, 0.2);
+
+var TypeProjection = function(targetType) {
+  this.targetType = targetType;
+  this.graph = TypeProjection.totalGraph.setFilter();
+  this.graph.calculateDistances();
+}
+
 
 /**
  * @brief Calculate distance from event to target type
  *
- * @return { name, distance } or undefined if no mapping was found.
+ * @return {name, distance, score} or undefined if no mapping was found.
  */
-Projection.prototype.map = function (object) {
+TypeProjection.prototype.map = function (object) {
   var d = 100000;
+  var score = object.score;
   var name;
 
   // Check the tags on this object
@@ -66,20 +94,24 @@ Projection.prototype.map = function (object) {
     if (tag.type === this.target) {
       d = 0;
       name = tag.name;
+      score = tag.score ? tag.score : score;
     }
     else {
       // Try different mappings
-
+      var graph = graphs[tag.type];
+      if (graph) {
+        var distance = graph.distance(tag.name
+      }
     }
   }
 
   if (name) {
-    return { distance: d, name: name };
+    return { distance: d, score: score, name: name };
   }
 }
 
 // Distance 0 == very important
-Projection.distanceToImportance = function(x) {
+TypeProjection.distanceToImportance = function(x) {
   var max = 1.0f; // how to find the max?
   var sigma = 1.0f;
   return max * Math.exp((x * x) / (2.0 * sigma * sigma));
@@ -87,14 +119,12 @@ Projection.distanceToImportance = function(x) {
 
 /**
  *
- */
-var Database = function() {
-  this.events = [
     {
       tags: [
         {
           'type': 'Person',
           'name': 'Fredrik Reinfeldt'
+          'score': 1
         },
         {
           'type': 'Topic',
@@ -104,7 +134,9 @@ var Database = function() {
       'createdAt': '2014-01-01',
       'score': 1
      }
-  ];
+ */
+var Database = function(events) {
+  this.events = events;
   this.filter = null;
 }
 
@@ -112,6 +144,7 @@ var Database = function() {
 /**
  * @brief Set filter which limits which events we are showing
  *
+ * Example:
  * @code
  * var filter = new Filter();
  * filter.equalTo('Topic', 'Skatt');
@@ -121,31 +154,74 @@ var Database = function() {
 Database.prototype.setFilter(filter) {
   this.filter = filter;
 }
+
 Database.prototype.getFilter(filter) {
   return this.filter;
 }
+
 Database.prototype.keys() {
   return ['Political Party', 'Topic', 'Person', 'Organization', 'Location'];
 }
+
 
 /**
  * @brief Project the events onto a main axis and calculate histogram.
  *
  * Example:
  * @code
- * var data = db.histogramBy('Topic', 'Political Party');
+ * var data = db.histogramBy('Topic');
  * data = {
- *  'Skatt': {'Socialdemorkaterna': 0.8, 'Moderaterna': 2, 'Miljöpartiet': 3},
- *  'Skola': {'Socialdemorkaterna': 3, 'Moderaterna': 4, 'Miljöpartiet': 7},
+ *  'Skatt': {negative: 5, positive: 7},
+ *  'Skola': {negative: 3, positive: 11},
  * }
  * @endcode
  */
-Database.prototype.histogramBy(mainAxis, secondaryAxis) {
+Database.prototype.histogramBy(mainAxis) {
+  // Get the projections
+  var mainProjection = new TypeProjection(mainAxis);
+
+  // Iterate over the events and project on the axis
+  for (var i = 0; i < events.length; i++) {
+    var e = events[i];
+    if (this.filter == null || this.filter.filter(e)) {
+      // This event should be included, project it onto the main and secondary
+      // axis.
+      var main = mainProjection.map(e);
+      if (main) {
+        // Add a result entry
+        var prevHistogramMap = result[main.name];
+        if (!prevHistogramArray) {
+          prevHistogramMap = {negative: 0, positive: 0};
+          result[main.name] = prevHistogramMap;
+        }
+        prevHistogramMap.negative += d;
+        prevHistogramMap.positive += d;
+      }
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * @brief Project the events onto a main axis and calculate histogram.
+ *
+ * Example:
+ * @code
+ * var data = db.projectionBy('Topic', 'Political Party');
+ * data = {
+ *  'Skatt': {'Socialdemokraterna': 0.8, 'Moderaterna': 2, 'Miljöpartiet': 3},
+ *  'Skola': {'Socialdemokraterna': 3, 'Moderaterna': 4, 'Miljöpartiet': 7},
+ * }
+ * @endcode
+ */
+Database.prototype.projectionBy(mainAxis, secondaryAxis) {
   var result = {};
 
   // Get the projections
-  var mainProjection = new Projection(mainAxis);
-  var secondaryProjection = new Projection(secondaryAxis);
+  var mainProjection = new TypeProjection(mainAxis);
+  var secondaryProjection = new TypeProjection(secondaryAxis);
 
   // Iterate over the events and project on the axis
   for (var i = 0; i < events.length; i++) {
@@ -176,6 +252,7 @@ Database.prototype.histogramBy(mainAxis, secondaryAxis) {
 
   return result;
 }
+
 
 /**
  * @brief Project the events onto a timeline
