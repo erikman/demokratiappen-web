8ca086a1bd0f66e725db04de228ccecdc529db0e
Erik Man <goodbyte@gmail.com>
2014-05-08 23:28:48 +0200

    Add d3 via bower and updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,116 @@
+'use strict';
+
 /*global module:false*/
 module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    appPath: require('./bower.json').appPath || 'app',
+    distPath: 'dist',
+
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= pkg.license %> */\n',
+
     // Task configuration.
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['<%= appPath %>/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: true
+        }
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= appPath %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= appPath %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+
+      /*copy: {
+        files: [
+   *///       'app/**/*',
+   //       '!app/js/*',
+   //        '!app/scripts/*'
+   /*     ],
+        tasks: ['copy']
+      },
+      concat: {
+   *///     files: [
+   //       'app/js/**/*.js',
+   //       '!app/js/bookmarklet.js'
+   /*     ],
+        tasks: ['concat']
+      },
+      uglify: {
+        files: [
+   *///       'dist/js/**/*.js',
+   //       '!dist/js/**/*.min.js'
+   /*     ],
+        tasks: ['uglify']
+      }*/
+
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            '<%= appPath %>'
+          ]
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          base: [
+            '.tmp',
+            'test',
+            '<%= appPath %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= distPath %>'
+        }
+      }
+    },
+
+    // Automatically inject Bower components into the app
+    'bower-install': {
+      app: {
+        html: '<%= appPath %>/index.html',
+        ignorePath: '<%= appPath %>/'
+      }
+    },
+
     tags: {
       dist: {
         options: {
@@ -18,9 +119,9 @@ module.exports = function(grunt) {
           closeTag: '<!-- end script template tags -->'
         },
         src: [
-          'dist/js/demokratiappen.min.js'
+          '<%= distPath %>/js/demokratiappen.min.js'
         ],
-        dest: 'dist/index.html'
+        dest: '<%= distPath %>/index.html'
       },
       development: {
         options: {
@@ -29,24 +130,71 @@ module.exports = function(grunt) {
           closeTag: '<!-- end script template tags -->'
         },
         src: [
-          'app/js/*.js',
-          '!app/js/bookmarklet.js'
+          '<%= appPath %>/js/*.js',
+          '!<%= appPath %>/js/bookmarklet.js'
         ],
-        dest: 'app/index.html'
+        dest: '<%= appPath %>/index.html'
       }
     },
+
+
+    // Allow the use of non-minsafe AngularJS files. Automatically makes it
+    // minsafe compatible so Uglify does not destroy the ng references
+    ngmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/concat/scripts',
+          src: '*.js',
+          dest: '.tmp/concat/scripts'
+        }]
+      }
+    },
+
+    // Replace Google CDN references
+    cdnify: {
+      dist: {
+        html: ['<%= yeoman.dist %>/*.html']
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
     copy: {
       dist: {
-        cwd: 'app',
-        src: ['**', '!js/*'],
-        dest: 'dist',
-        expand: true
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= appPath %>',
+          dest: '<%= distPath %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            '*.html',
+            'views/{,*/}*.html',
+            'bower_components/**/*',
+            'images/{,*/}*.{webp}',
+            'fonts/*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= distPath %>/images',
+          src: ['generated/*']
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= appPath %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
       }
     },
+
     clean: {
       dist: {
-        src: ['dist']
+        src: ['<%= distPath %>']
       },
+      server: '.tmp'
     },
     concat: {
       options: {
@@ -55,10 +203,10 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [
-          'app/js/**/*.js',
-          '!app/js/bookmarklet.js'
+          '<%= appPath %>/js/**/*.js',
+          '!<%= appPath %>/js/bookmarklet.js'
         ],
-        dest: 'dist/js/<%= pkg.name %>.js'
+        dest: '<%= distPath %>/js/<%= pkg.name %>.js'
       }
     },
     uglify: {
@@ -101,48 +249,61 @@ module.exports = function(grunt) {
         src: ['app/**/*.js']
       }
     },
-    watch: {
-      copy: {
-        files: [
-          'app/**/*',
-          '!app/js/*',
-          '!app/scripts/*'
-        ],
-        tasks: ['copy']
-      },
-      concat: {
-        files: [
-          'app/js/**/*.js',
-          '!app/js/bookmarklet.js'
-        ],
-        tasks: ['concat']
-      },
-      uglify: {
-        files: [
-          'dist/js/**/*.js',
-          '!dist/js/**/*.min.js'
-        ],
-        tasks: ['uglify']
-      }
-    }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  /*grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-script-link-tags');
+  grunt.loadNpmTasks('grunt-script-link-tags');*/
+
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+//      'bower-install',
+//      'concurrent:server',
+//      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('build', [
+     'clean',
+     'copy',
+     'concat',
+     'uglify',
+     'tags'
+ /*   'clean:dist',
+    'bower-install',
+    'useminPrepare',
+    'concurrent:dist',
+    'autoprefixer',
+    'concat',
+    'ngmin',
+    'copy:dist',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'rev',
+    'usemin',
+    'htmlmin'*/
+  ]);
 
   // Default task.
-  grunt.registerTask('default', [
-    'clean',
+  grunt.registerTask('default', [ /*'jshint' */ 'build'
+  /*  'clean',
     'copy',
     'concat',
     'uglify',
-    'tags'
+    'tags'*/
   ]);
 
 };
---
+++ app/js/dataprojection.js
@@ -0,0 +1,315 @@
+!function() {
+  'use strict';
+
+  var dp = {
+    version: "0.0.0"
+  };
+
+/**
+ * @brief This class describes one event created by the user.
+ *
+ * Example:
+ * @code
+ * var e = new Event();
+ * e.addTag({type: 'Topic', name: 'Skatt'}); // Note no score
+ * e.addTag({type: 'Political Party', name: 'Socialdemokraterna', score: -1});
+ * e.addTag({type: 'Political Party', name: 'Vänsterpartiet', score: 1});
+ * e.addTag({type: 'Person', name: 'Stefan Löfgren', score: -1});
+ * @endcode
+ */
+dp.Event = function() {
+  this.types = {};
+};
+
+
+dp.Event.prototype.addTag = function(tag) {
+  var types = this.types[tag.type];
+  if (!types) {
+    types = [];
+    this.types = types;
+  }
+
+  types[tag.name] = {
+    score: tag.score
+  };
+}
+
+
+/**
+ * @brief The tag types we allow.
+ */
+dp.Event.prototype.keys = function() {
+  return ['Political Party', 'Topic', 'Person', 'Organization', 'Location'];
+}
+
+
+/**
+ * @brief Internal class used for creating event filters
+ */
+var EventFilterOP = {};
+
+EventFilterOP.Equals = function(type, value) {
+  this.type = type;
+  this.value = value;
+}
+EventFilterOP.Equals.prototype.filter = function(object) {
+  var types = object.types[this.type];
+  var value;
+  if (types) {
+    value = types[this.value];
+  }
+  return !!value;
+}
+
+
+/**
+ * @brief Event filter class
+ *
+ * This is basically an array of event filter operations and some convenience functions
+ * for creating them in a programmer friendly way.
+ */
+dp.EventFilter = function() {
+  this.filters = [];
+}
+dp.EventFilter.prototype.equalTo = function(type, value) {
+  this.filters[this.filters.length] = new FilterOP.Equals(type, value);
+}
+dp.EventFilter.prototype.filter = function(object) {
+  var result = true;
+  for (var i = 0; result && (i < this.filters.length); i++) {
+    result &= this.filters[i].filter(object);
+  }
+  return result;
+}
+
+
+/**
+ * @brief Class for projecting an event onto a type axis
+ */
+var TypeProjection = function(type) {
+  this.type = type;
+}
+
+
+/**
+ * @brief Calculate distance from event to target type
+ *
+ * @return {name, distance, score} or undefined if no mapping was found.
+ */
+TypeProjection.prototype.map = function(object) {
+  // Check for direct result of this event
+  var results = [];
+  var typeTags = object.tags[this.type];
+  if (typeTags) {
+    for (var key in typeTags) {
+      var score = typeTags[key].score;
+      if (!score) {
+        score = object.score;
+      }
+
+      results[results.length] = {
+        type: this.type,
+        name: key,
+        score: score,
+        distance: 0
+      };
+    }
+  }
+
+  // TODO: If no direct results are found, try to map the object onto the main axis.
+  return results;
+}
+
+
+// Distance 0 == very important
+TypeProjection.distanceToImportance = function(x) {
+  var sigma = 1.0; // How to find distance form the graph?
+  return Math.exp((x * x) / (2.0 * sigma * sigma));
+}
+
+
+/**
+ * @brief The data base connection we will make queries against.
+ */
+dp.Database = function(events) {
+  this.events = events;
+  this.filter = null;
+}
+
+
+/**
+ * @brief Set filter which limits which events we are showing
+ *
+ * Example:
+ * @code
+ * var filter = new Filter();
+ * filter.equalTo('Topic', 'Skatt');
+ * db.setFilter(filter);
+ * @endcode
+ */
+dp.Database.prototype.setFilter = function(filter) {
+  this.filter = filter;
+}
+
+dp.Database.prototype.getFilter = function(filter) {
+  return this.filter;
+}
+
+
+/**
+ * @brief Project the events onto a main axis and calculate histogram.
+ *
+ * Example:
+ * @code
+ * var data = db.histogramBy('Topic');
+ * data = {
+ *  'Skatt': {negative: 5, positive: 7, count: 12},
+ *  'Skola': {negative: 3, positive: 11, count 15}, // count can be > negative+positive if events are missing scores
+ * }
+ * @endcode
+ */
+dp.Database.prototype.histogramBy = function(mainAxis) {
+  // Get the projections
+  var mainProjection = new dp.TypeProjection(mainAxis);
+
+  // Iterate over the events and project on the axis
+  for (var i = 0; i < events.length; i++) {
+    var e = events[i];
+    if (this.filter == null || this.filter.filter(e)) {
+      // This event should be included, project it onto the main and secondary
+      // axis.
+      var main = mainProjection.map(e);
+      if (main) {
+        for (var j = 0; j < main.length; j++) {
+          // Add a result entry
+          var prevHistogramMap = result[main.name];
+          if (!prevHistogramArray) {
+            prevHistogramMap = {negative: 0, positive: 0};
+            result[main.name] = prevHistogramMap;
+          }
+
+          var importance = TypeProjection.distanceToImportance(main.distance);
+          prevHistogramMap.count += importance;
+          if (main.score < 0) {
+            prevHistogramMap.negative += -main.score * importance;
+          }
+          else if (main.score > 0) {
+            prevHistogramMap.positive += main.score * importance;
+          }
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * @brief Project the events onto a main axis and calculate histogram.
+ *
+ * Example:
+ * @code
+ * var data = db.projectionBy('Topic', 'Political Party');
+ * data = {
+ *  'Skatt': {'Socialdemokraterna': 0.8, 'Moderaterna': 2, 'Miljöpartiet': 3},
+ *  'Skola': {'Socialdemokraterna': 3, 'Moderaterna': 4, 'Miljöpartiet': 7},
+ * }
+ * @endcode
+ */
+dp.Database.prototype.projectionBy = function(mainAxis, secondaryAxis) {
+  var result = {};
+
+  // Get the projections
+  var mainProjection = new TypeProjection(mainAxis);
+  var secondaryProjection = new TypeProjection(secondaryAxis);
+
+  // Iterate over the events and project on the axis
+  for (var i = 0; i < events.length; i++) {
+    var e = events[i];
+    if (this.filter == null || this.filter.filter(e)) {
+      // This event should be included, project it onto the main and secondary
+      // axis.
+      var main = mainProjection.map(e);
+      var second = secondaryProjection.map(e);
+      if (main && second) {
+        // TODO: Form the different tag combinations
+
+        // Combine the distances
+        var d = Math.sqrt(main.distance * main.distance
+                          + second.distance * second.distance);
+
+        // Add a result entry
+        var prevHistogramMap = result[main.name];
+        if (!prevHistogramArray) {
+          prevHistogramMap = {};
+          result[main.name] = prevHistogramMap;
+        }
+        var prevHistogramValue = prevHistogramMap[second.name];
+        if (!prevHistogramValue) prevHistogramValue = 0;
+        prevHistogramMap[second.name] = prevHistogramValue
+          + e.score * Projection.distanceToImportance(d);
+      }
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * @brief Project the events onto a timeline
+ *
+ * @code
+ * data = graph.timelineBy('Political Party');
+ * data = {
+ *   'Socialdemokraterna': [{date: 2014-01-01, value: 1}, ... ],
+ *   'Moderaterna': [...],
+ * };
+ * @endcode
+ */
+dp.Database.prototype.timelineBy = function(mainAxis) {
+  var result = {};
+
+  // Get the projections
+  var projection = new Projection(mainAxis);
+
+  // Iterate over the events and project on the axis
+  for (var i = 0; i < events.length; i++) {
+    var e = events[i];
+    if (this.filter == null || this.filter.filter(e)) {
+      // This event should be included, project it onto the main and secondary
+      // axis.
+      var main = mainProjection.map(e);
+      if (main) {
+        // Add a result entries
+        for (var j = 0; j < main.length; j++) {
+          var timeline = result[main];
+          if (!timeline) {
+            timeline = [];
+            result[main] = timeline;
+          }
+
+          timeline[timeline.length] = {
+            date: e.createdA,
+            type: mainAxis,
+            name: main[j].name,
+            importance: main[j].score * Projection.distanceToImportance(main[j].distance)
+          };
+        }
+      }
+    }
+  }
+
+  // TODO: Sort the timelines
+  return result;
+}
+
+  if (typeof define === "function" && define.amd) {
+    define(dp);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = dp;
+  } else if (this) {
+    this.dp = dp;
+  }
+}();
